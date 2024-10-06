76062dc235ac9e76fc569ff8ec42dbc2321d1659
zain <i232582@isb.nu.edu.pk>
2024-10-06 22:34:55 +0500

    GameUpdate:Grid and Movement function is added

--- 23i2582_DS3A.cpp
+++ 23i2582_DS3A.cpp
@@ -1,6 +1,177 @@
 #include <iostream>
 using namespace std;
-int main()
+
+class node 
 {
+public:
+    char data;
+   node *down;
+   node *up;
+   node *left;
+   node *right;
+    
+    node(char data) 
+    {
+        this->data = data;
+      this->down=NULL;
+      this->left=NULL;
+      this->up=NULL;
+      this->right=NULL;
+    }
+};
+
+class grid 
+{
+public:
+    node *head;
+    grid() 
+    {
+        head = NULL;
+    }
+    void creategrid(int rows, int cols) 
+    {
+        if (rows == 0 || cols == 0) return;
+        head = new node('#');  
+        node* temp = head;     
+        node* prevrow = NULL;  
+        for (int i = 0; i < rows; i++) 
+        {
+            node* rowhead = temp;  
+            for (int j = 1; j < cols; j++) 
+            {
+                node* newNode = new node('#');
+                temp->right = newNode;
+                newNode->left = temp;
+                temp = temp->right;
+            }
+            
+            if (prevrow) 
+            {
+                node* tempupper = prevrow;
+                node* templower = rowhead; 
+                while (tempupper && templower) 
+                {
+                    tempupper->down = templower;
+                    templower->up = tempupper;
+                    tempupper = tempupper->right;
+                    templower = templower->right;
+                }
+            }
+            if (i < rows - 1) 
+            {
+                node* nextRow = new node('#');
+                rowhead->down = nextRow;
+                nextRow->up = rowhead;
+                temp = nextRow;
+                prevrow = rowhead;
+            }
+        }
+    }
+    
+    void print() 
+    {
+        node* row = head;
+        while (row != nullptr) 
+        {
+            node* temp = row;
+            while (temp != nullptr) 
+            {
+                cout << temp->data << " ";
+                temp = temp->right;
+            }
+            
+            cout << endl;
+            row = row->down;
+        }
+        cout<<endl;
+    }
+};
+class playermovement {
+    public:
+    node *position;
+    
+    playermovement(grid &g)
+    {
+        position=g.head;
+        position->data='P';
+    }
+    void leftmove()
+    {
+        if(position->left==NULL)
+        {
+            cout<<"invalid position"<<endl;
+            return ;
+        }
+        if(position->left!=NULL)
+        {
+           
+            position->data='#';
+            position=position->left;
+            position->data='P';
+        }
+    }
+     void rightmove()
+    {
+        if(position->right==NULL)
+        {
+            cout<<"invalid position"<<endl;
+            return ;
+        }
+        if(position->right!=NULL)
+        {
+           
+            position->data='#';
+            position=position->right;
+            position->data='P';
+        }
+    }
+    void upmove()
+    {
+        if(position->up==NULL)
+        {
+            cout<<"invalid position"<<endl;
+            return ;
+        }
+        if(position->up!=NULL)
+        {
+           
+            position->data='#';
+            position=position->up;
+            position->data='P';
+        }
+    }
+    void downmove()
+    {
+        if(position->down==NULL)
+        {
+            cout<<"invalid position"<<endl;
+            return ;
+        }
+        if(position->down!=NULL)
+        {
+           
+            position->data='#';
+            position=position->down;
+            position->data='P';
+        }
+    }
+
+    
+};
+int main() 
+{
+    grid g;
+    g.creategrid(10, 10);
+    g.print();
+    playermovement m(g);
+    m.downmove();
+    g.print();
+    m.rightmove();
+    g.print();
+    m.leftmove();
+    g.print();
+    m.upmove();
+    g.print();
+    
     return 0;
 }
